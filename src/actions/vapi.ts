71fa73950df95626b73a2d8dc89ff9ce3419d450
Tanmay <tanmayprajapat589@gmail.com>
2026-04-26 21:49:07 +0530

    added ai agent fully complete

--- src/actions/vapi.ts
+++ src/actions/vapi.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { aiAgentPrompt } from "@/lib/data"
 import { vapiServer } from "@/lib/vapi/vapiServer"
 
 export const getAllAssistants = async () => {
@@ -19,3 +20,81 @@ export const getAllAssistants = async () => {
         }
     }
 }
+
+
+export const createAssistant = async (name: string) => {
+    try {
+        const createAssistant = await vapiServer.assistants.create({
+            name,
+            firstMessage: `Hi there, this is ${name} from customer support. How can I help you today?`,
+            model: {
+                model: 'gpt-4o',
+                provider: 'openai',
+                messages: [
+                    {
+                        role: 'system',
+                        content: aiAgentPrompt,
+                    },
+                ],
+                temperature: 0.5,
+            },
+        })
+        return {
+            success: true,
+            status: 200,
+            data: createAssistant,
+        }
+    } catch (error) {
+        console.error('Error creating assistant:', error)
+        return{
+            success: false,
+            status: 500,
+            message: "Failed to create assistant",
+        }
+    }
+}
+
+
+
+export const updateAssistant = async (
+    assistantId: string,
+    {
+        firstMessage,
+        systemPrompt,
+    }: {
+        firstMessage: string
+        systemPrompt: string
+    }
+) => {
+    try {
+        const updateAssistant = await vapiServer.assistants.update(assistantId, {
+            firstMessage,
+            model: {
+                model: 'gpt-4o',
+                provider: 'openai',
+                messages: [
+                    {
+                        role: 'system',
+                        content: systemPrompt,
+                    },
+                ],
+            },
+        })
+
+        console.log('Assistant updated:', updateAssistant)
+
+        return {
+            success: true,
+            status: 200,
+            data: updateAssistant,
+        }
+    } catch (error) {
+        console.error('Error updating assistant:', error)
+        return {
+            success: false,
+            status: 500,
+            message: 'Failed to update assistant',
+        }
+    }
+}
+
